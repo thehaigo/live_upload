3e58c57626e450fb63ee4f73f89a48d2cc470165
thehaigo <sirannha@gmail.com>
2022-05-25 12:05:34 +0900

    / access redirect to articles

--- lib/live_upload_web/controllers/page_controller.ex
+++ lib/live_upload_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule LiveUploadWeb.PageController do
   use LiveUploadWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    redirect(conn, to: Routes.article_index_path(LiveUploadWeb.Endpoint, :index))
   end
 end
